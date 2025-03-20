c1aab478033d4b96d05351e5ce336b78f295f505
HARISANKARK <hari404832@gmail.com>
2025-03-20 22:15:23 +0530

    Products getting api filter setting

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -7,9 +7,26 @@ use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    public function ViewProducts()
+    public function ViewProducts(Request $request)
     {
-        $products = Product::all();
+        $query = Product::query();
+
+        if($request->has('name')){
+            $query->where('name','LIKE','%'.$request->name.'%');
+        }
+        if($request->has('description')){
+            $query->where('description','LIKE','%'.$request->description.'%');
+        }
+
+        if($request->has('min_price')){
+            $query->where('price','>=',$request->min_price);
+        }
+
+        if($request->has('max_price')){
+            $query->where('price','<=',$request->max_price);
+        }
+
+        $products = $query->get();
 
         if (!$products->isEmpty()) {
             return response()->json([
